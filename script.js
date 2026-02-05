1af957a1285a6dd184d8c568d235969a2df3085b
Czarinah Malazo-Llapitan <odiecza@gmail.com>
2026-02-05 19:15:50 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll offset for fixed navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    const offset = 80;
+
+    window.scrollTo({
+      top: target.offsetTop - offset,
+      behavior: 'smooth'
+    });
+  });
+});
